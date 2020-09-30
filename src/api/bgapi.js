89de1ca87672031f9f500a4cf9662c78b668d6d1
郭海潮 <guohaichao@longshine.com>
2020-10-01 00:00:52 +0800

    update

--- src/api/bgapi.js
+++ src/api/bgapi.js
@@ -1,5 +1,4 @@
 const host = process.env.VUE_APP_BASE_API
 export default {
   menu: `${host}/api/menu`,
-  menu: `${host}/api/menu`
 }
--- src/views/system/menu.vue
+++ src/views/system/menu.vue
@@ -1,121 +1,46 @@
 <template>
 <div class="app-container">
   <header class="app-header">
-    <span class="title">网校权限管理</span>
+    <span class="title">{{$route.meta.title}}</span>
     <el-button type="primary" icon="el-icon-edit" @click.native="handleAddFirstCategory()">添加一级分类</el-button>
   </header>
   <div class="course-class">
     <div class="class-main">
-        <div class="classify" style="padding-left:10px; padding-top:2px;">
-          <el-tree :data="data" node-key="id" v-loading="loading" default-expand-all :expand-on-click-node="false" :props="defaultProps">
-            <span style="flex: 1; display: flex; align-items: center; justify-content: space-between; padding-right: 8px;"  slot-scope="{ node, data }">
-                <span>{{ node.label }}</span>
-                <span class="operation-col">
-                  <input class="order-list" @blur ="() => setOrder(data)" v-model="data.list_order">
-                  <el-button type="text" @click.native.prevent ="() => append(data)">添加子分类</el-button>
-                  <el-button type="text" @click="() => edit(data)">编辑</el-button>
-                  <el-button type="text" @click="() => remove(data)">删除</el-button>
-                </span>
-            </span>
-          </el-tree>
-        </div>      
+      <div class="classify">
+        <div class="tree-top">
+          <span class="tree-title" style="padding-left:40px;">菜单名称</span>
+          <span class="tree-name">菜单文件名称</span>
+            <span class="tree-order">排序</span>
+            <span class="tree-btns">操作</span>
+        </div> 
+        <el-tree :data="data" node-key="id" v-loading="loading" default-expand-all :expand-on-click-node="false" :props="defaultProps">
+          <div class="tree-li" slot-scope="{ node, data }">
+              <span v-show="!data.extend" class="tree-title">{{ data.menuTitle }}</span>
+              <span v-show="data.extend" class="tree-title">
+                <el-input style=" width:80%" v-show="data.extend"  placeholder="请输入中文名称" size="mini" v-model.trim="data.menuTitle"  auto-complete="off"></el-input>
+              </span>
+              <span v-show="!data.extend" class="tree-name">{{ data.menuName }}</span>
+              <span v-show="data.extend" class="tree-name">
+                <el-input style=" width:80%" size="mini" v-model.trim="data.menuName" placeholder="请输入英文名称" auto-complete="off"></el-input>
+              </span>
+              <span class="tree-order">
+                <el-input-number size="mini" :min="0" @change = "() => updateOrder(data)"  v-model="data.orderNo"></el-input-number>
+              </span>
+              <span class="tree-btns">  
+              <el-button-group>
+                <el-button size="mini" type="primary" @click.native.prevent ="() => append(data)">添加子分类</el-button>
+                <el-button v-show="data.extend" size="mini" type="warning" @click="() => edit(data)">保存</el-button>
+                <el-button v-show="!data.extend" size="mini" type="primary" @click="() => edit(data)">编辑</el-button>
+                <el-button size="mini" type="danger"  @click="() => remove(data,node)">删除</el-button>
+              </el-button-group>
+              </span>
+          </div>
+        </el-tree>
+      </div>      
     </div>
-
-    <el-dialog title="添加一级分类" :visible.sync="formVisible" :before-close="hideForm" :close-on-click-modal="false" width="50%" top="5vh">
-      <el-form  :model="formData" :key="1" :rules="formRules" ref="dataForm" label-width="80px" style="padding-right:25px;">
-        <el-form-item label="是否显示" prop="type">
-          <el-radio-group v-model="formData.status">
-              <el-radio label="1">显示</el-radio>
-              <el-radio label="0">隐藏</el-radio>
-          </el-radio-group>   
-        </el-form-item>
-        <el-form-item label="菜单类型" prop="type">
-          <el-radio-group v-model="formData.type">
-              <el-radio label="1">列表显示</el-radio>
-              <el-radio label="2">列表隐藏</el-radio>
-          </el-radio-group>   
-        </el-form-item>      
-        <el-form-item label="分类名称" prop="name">
-          <el-input v-model="formData.name" placeholder="分类名称" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="所属路径" prop="path">
-          <el-input v-model="formData.path" placeholder="所属路径" auto-complete="off"></el-input>
-        </el-form-item>              
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-          <el-button @click.native="hideForm">取消</el-button>
-          <el-button type="primary" @click.native="formSubmit()" :loading="formLoading">提交</el-button>
-      </div>
-    </el-dialog>
-
-    <el-dialog title="添加子分类" :visible.sync="subCategoryFormVisible" :before-close="hideSubCategoryForm" :close-on-click-modal="false" width="50%" top="5vh">
-      <el-form  :model="subCategoryFormData" :key="2" :rules="formRules" ref="dataForm" label-width="80px" style="padding-right:25px;">
-        <el-form-item label="是否显示" prop="status">
-          <el-radio-group v-model="subCategoryFormData.status">
-              <el-radio label="1">显示</el-radio>
-              <el-radio label="0">隐藏</el-radio>
-          </el-radio-group>   
-        </el-form-item>  
-        <el-form-item label="菜单类型" prop="type">
-          <el-radio-group v-model="subCategoryFormData.type">
-              <el-radio label="1">列表显示</el-radio>
-              <el-radio label="2">列表隐藏</el-radio>
-          </el-radio-group>   
-        </el-form-item>
-        <el-form-item label="所属分类" prop="parent_name">
-          <el-input v-model="subCategoryFormData.parent_name" disabled placeholder="父分类名称" auto-complete="off"></el-input>
-        </el-form-item>              
-        <el-form-item label="分类名称" prop="name">
-          <el-input v-model="subCategoryFormData.name" placeholder="分类名称" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="所属路径" prop="path">
-          <el-input v-model="subCategoryFormData.path" placeholder="所属路径" auto-complete="off"></el-input>
-        </el-form-item>              
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-          <el-button @click.native="hideSubCategoryForm">取消</el-button>
-          <el-button type="primary" @click.native="subCategoryFormSubmit()" :loading="formLoading">提交</el-button>
-      </div>
-    </el-dialog>
-
-
-    <el-dialog title="编辑" :visible.sync="curCategoryFormVisible" :before-close="hideCurCategoryForm" :close-on-click-modal="false" width="50%" top="5vh">
-      <el-form  :model="curCategoryFormData" :key="3"  label-width="80px" style="padding-right:25px;">
-        <el-form-item label="是否显示">
-          <el-radio-group v-model="curCategoryFormData.status">
-              <el-radio label="1">显示</el-radio>
-              <el-radio label="0">隐藏</el-radio>
-          </el-radio-group>   
-        </el-form-item>       
-        <el-form-item label="菜单类型">
-          <el-radio-group v-model="curCategoryFormData.type">
-              <el-radio label="1">列表显示</el-radio>
-              <el-radio label="2">列表隐藏</el-radio>
-          </el-radio-group>   
-        </el-form-item>             
-        <el-form-item label="分类名称" prop="name">
-          <el-input v-model="curCategoryFormData.name" placeholder="分类名称" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="所属路径" prop="path">
-          <el-input v-model="curCategoryFormData.path" placeholder="所属路径" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="选中路径" prop="active_path">
-          <el-input v-model="curCategoryFormData.active_path" placeholder="所选中路径/各父元素path" auto-complete="off"></el-input>
-        </el-form-item>         
-        
-                      
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-          <el-button @click.native="hideCurCategoryForm">取消</el-button>
-          <el-button type="primary" @click.native="curCategoryFormSubmit()" :loading="formLoading">提交</el-button>
-      </div>
-    </el-dialog>
-
-
   </div>
 </div>
 </template>
-
 <script>
 import config from '@/api/bgapi'
 import request from '@/utils/request'
@@ -124,175 +49,89 @@ export default {
     return {
       loading: false,
       data:[],
-      defaultProps: {children: "children", label: "title"},
-      formVisible:false,
-      formData: {parent_id:'0',type:'1',status:'1', name:'',path:''},
-      formRules: {
-        name: [{ required: true, message: "请输入分类名称", trigger: "blur" }],
-        path: [{ required: true, message: "请输入分类所在路径", trigger: "blur" }]
-      },
-      formRulesEdit:{
-        name: [{ required: true, message: "请输入分类名称", trigger: "blur" }],
-        path: [{ required: true, message: "请输入分类所在路径", trigger: "blur" }]
-      },
-      formLoading:false,
-      subCategoryFormVisible:false,
-      subCategoryFormData:{parent_id:'',type:'1',status:'1',name:'',path:''},
-      curCategoryFormVisible:false,
-      curCategoryFormData:{type:'',name:'',status:'',path:''},
+      defaultProps: {children: "children", label: "menuTitle"},
+      formLoading:false
     }
   },
   mounted(){
-   // this.getList();
+    this.getList();
   },
   methods: {
     async getList() {
       this.loading = true; 
-      let res = await request.get(config.listUrl); 
+      let res = await request.get(config.menu); 
       this.loading = false;
       if(res.code == '20000'){
-        this.data = res.data.list;
+        this.data = res.data;
       }
     },
     handleAddFirstCategory() {
-      this.formVisible = true;
-    },
-    hideForm(){
-      this.formVisible = !this.formVisible;
-      this.resetForm();
-      return true;     
-    },
-    resetForm() {
-      if (this.$refs["dataForm"]) {
-        this.$refs["dataForm"].clearValidate();
-        this.$refs["dataForm"].resetFields();
-      }
-    },
-    formSubmit(){
-      this.$refs["dataForm"].validate(async valid => {
-        if (valid) {
-          this.formLoading = true;
-          var res = await request.post(config.addMenuSave,this.formData); 
-          this.formLoading = false;
-          if(res.code == '20000'){
-            this.hideForm();
-            this.$message.success("分类添加成功"); 
-            this.getList();	
-          }          
-        }
-      });
-    },
-    hideSubCategoryForm() {
-      this.subCategoryFormVisible = !this.subCategoryFormVisible;
-      this.resetForm()
-      return true;
+      var obj = {parentId:'0', orderNo:0, hidden:false, menuName:'',menuTitle:'',pathIds:['0'],extend:true};
+      this.data.push(obj)
     },
     append(data) {
-      this.subCategoryFormVisible = true;
-      this.subCategoryFormData =  {parent_name:data.title,status:'1',parent_id:data.id, type:'1', name: '', path:''}
+      const newChild = {parentId:data.id, orderNo:0, hidden:false, menuName:'',menuTitle:'',pathIds:[...data.pathIds,data.id],extend:true};
+      if (!data.children) {
+        this.$set(data, 'children', []);
+      }
+      data.children.push(newChild);
     },
-    subCategoryFormSubmit(){
-       this.$refs["dataForm"].validate(async valid => {
-        if (valid) {
-          this.formLoading = true;
-          var res = await request.post(config.addMenuSave,this.subCategoryFormData); 
-          this.formLoading = false;
-          if(res.code == '20000'){
-            this.hideSubCategoryForm();
-            this.$message.success("分类添加成功"); 
+    remove(data,node) {
+      if(data.id){
+        this.$confirm("确认删除当前分类吗?", "提示", {type: "warning",}).then(async () => {
+          var res = await request.delete(config.menu+'/'+data.id);
+          if(res && res.code == '20000'){
+            this.$message.success("删除成功");  
             this.getList();	
-          }          
-        }
-      });     
-    },
-    remove(data) {
-      this.$confirm("确认删除当前分类吗?", "提示", {
-        type: "warning",
-      })
-      .then(async () => {
-         var res = await request.post(config.delMenuUrl,{'id':data.id});
-         if(res && res.code == '20000'){
-           this.$message.success("删除成功");  
-           this.getList();	
-         }
-      });
-    },
-    async edit(data){   
-      let res = await request.get(config.detUrl,{params:{id:data.id}});
-      if(res.code == '20000'){
-        this.curCategoryFormVisible = true;
-        let _info = res.data && res.data.info ? res.data.info : '';
-        if(_info){
-          this.curCategoryFormData =  { 
-            id:_info.id,
-            parent_id:_info.parent_id,
-            type:_info.type+'', 
-            status:_info.status+'',
-            name:_info.name, 
-            path:_info.path,
-						active_path:_info.active_path
           }
-        }
+        });
+      }else{
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === data.id);
+        children.splice(index, 1);
       }
     },
-    hideCurCategoryForm(){
-      this.curCategoryFormVisible = !this.curCategoryFormVisible;
-      this.resetForm()
-      return true;     
-    },
-    async curCategoryFormSubmit(){
-      // this.$refs["dataForm"].validate(async valid => {
-     //   if (valid) {
-        this.formLoading = true;
-        var res = await request.post(config.editUrl,this.curCategoryFormData); 
-        this.formLoading = false;
-        if(res.code == '20000'){
-          this.hideCurCategoryForm();
-          this.$message.success("分类编辑成功"); 
-          this.getList();	
-        }          
-      //  }
-    //  });       
-    },
-    async setOrder(data){
-      var params = {id:data.id,list_order:data.list_order}
-      var res = await request.post(config.sortUrl,params); 
-      if(res.code == '20000'){
-        this.$message.success("分类排序成功"); 
-        this.getList();	
+    async updateOrder(data){
+      var _data = {orderNo:data.orderNo};
+      var res = await request.put(config.menu+'/'+data.id,_data);
+      if(res && res.code == '20000'){
+        this.getList();
+      }
+		},
+    async edit(data){
+      if(data.extend){
+        if(data.menuTitle.length < 3 || data.menuName.length < 3){
+          this.$message.error("菜单名称不少于3个字符！"); 	
+        }else{
+          var _id = data.id;
+          if(_id){
+            var _obj = {
+              menuName:data.menuName,
+              menuTitle:data.menuTitle,					
+            }
+            await request.put(config.menu+'/'+_id,_obj); 	
+          }else{
+            await request.post(config.menu,data);	
+          }
+          data.extend = !data.extend;
+        }
+      }else{
+        data.extend = !data.extend;
       }
     }
   }
 }
 </script>
 <style  scoped>
-.app-header{ 
-    padding:0; 
-    height:50px;  
-    overflow:hidden; 
-    text-align:right;
-}
-.app-header .title{ 
-    float:left; 
-    font-size:23px; 
-    border-left:3px solid #1f2f3d; 
-    padding-left:10px; 
-    vertical-align:middle;
-}
-.query-form{ 
-    background:#FAFAFA; 
-    border-radius:4px; 
-    padding:23px 0 0 20px; 
-    margin-bottom:10px;
-}
-.dialog-footer{ padding-right: 20px;}
-.order-list{ display:inline-block; width:80px; margin-right:10px; border-radius: 2px; height:24px; line-height:24px; border:1px solid #66B1FF; color:#66B1FF; text-align: center;}
-.order-list:focus { display:inline-block; width:80px; margin-right:10px; border-radius: 2px; height:24px; line-height:24px; border:1px solid #66B1FF; color:#66B1FF; text-align: center;}
-.course-class>>>  .classify >>> .operation-col >>> .el-tree-node__content {
-      height: 32px;
-      margin: 0; 
-      line-height: 32px; font-size: 14px;
-      &:hover .operation-col {}
-}
-
+.classify{border:1px solid #e1e1e1; border-bottom:0; min-width:800px;}
+.classify >>> .el-tree-node__content{height: 40px; border-bottom: 1px solid #e1e1e1;line-height: 40px;}
+.classify >>> .el-tree-node__expand-icon{ padding:0 15px;}
+.classify >>> .el-tree__empty-block{ border-bottom: 1px solid #e1e1e1;}
+.tree-li{flex: 1; display: flex; align-items: center; font-size:14px; justify-content: space-between;}
+.tree-title{flex:1; border-right:1px solid #e1e1e1;}
+.tree-name{width:200px; border-right:1px solid #e1e1e1; text-align:center;}
+.tree-order{width:200px; border-right:1px solid #e1e1e1; text-align:center;}
+.tree-btns{ width:250px; text-align:center;}
+.tree-top{height: 42px; display:flex; font-size:14px; font-weight:bold; border-bottom: 1px solid #e1e1e1;line-height: 42px; background:#F5F7FA}
 </style>
