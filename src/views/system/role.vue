e3f703503c5b9b1713d3acb03e7320687556b3e8
郭海潮 <guohaichao@longshine.com>
2020-10-03 07:40:02 +0800

    updte

--- src/views/system/role.vue
+++ src/views/system/role.vue
@@ -8,7 +8,7 @@
       <el-form-item class="query-form-item">
         <el-autocomplete
            class="inline-input"
-          v-model="listQuery.name"
+          v-model="listQuery.roleName"
           :fetch-suggestions="querySearch"
           placeholder="请输入角色名称"
           :trigger-on-focus="false"
@@ -16,7 +16,7 @@
           @select="handleSelect"
         >
         <template slot-scope="{ item }">
-          <div class="name">{{ item.name }}</div>
+          <div class="name">{{ item.roleName }}</div>
         </template>
         </el-autocomplete>
       </el-form-item>
@@ -33,25 +33,17 @@
         </el-button-group>
       </el-form-item>
     </el-form>
+    
     <el-table v-loading="loading" :data="list">
       <el-table-column label="角色 ID" prop="id"/>
-      <el-table-column label="角色名称" prop="name"/>
+      <el-table-column label="角色名称" prop="roleName"/>
       <el-table-column label="状态">
         <template slot-scope="scope">
-          <template v-if="scope.row.name == '超级管理员'">
-            <el-tag  type="danger">系统</el-tag>
-          </template>
-          <template v-else>
             <el-tag :type="scope.row.status | statusFilterType">{{ scope.row.status | statusFilterName }}</el-tag>
-          </template>
         </template>
       </el-table-column>
-      <el-table-column label="描述" :show-overflow-tooltip="true">
-        <template slot-scope="scope">
-        	<span v-if="scope.row.name == '超级管理员'" style="color:#CCC">该角色拥用所有限权不允许编辑</span>
-          <span v-else>{{ scope.row.remark }}</span>
-        </template>
-      </el-table-column>
+      <el-table-column label="描述" prop="roleRemark" :show-overflow-tooltip="true" />
+     
       <el-table-column label="操作"  width="200">     
         <template slot-scope="scope">
           <template v-if="scope.row.name == '超级管理员'">
@@ -60,7 +52,7 @@
             <el-button type="text" disabled>删除</el-button>
         	</template>
           <template v-else>
-            <el-button type="text" @click.native="handleAuth(scope.row.id)">授权</el-button>
+            <el-button type="text" @click.native="handleAuth(scope.row)">授权</el-button>
             <el-button type="text" @click.native="handleForm(scope.$index, scope.row)">编辑</el-button>
             <el-button type="text" @click.native="handleDel(scope.$index, scope.row)">删除</el-button>
           </template>
@@ -69,22 +61,21 @@
       </el-table-column>    
     </el-table>
 
-    <pagination v-show="total > listQuery.page_size" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.page_size" @pagination="getList" />
-
+ 
     <!--表单-->
     <el-dialog :title="formName" :visible.sync="formVisible" :before-close="hideForm" :close-on-click-modal="false" width="50%" top="5vh">
         <el-form :model="formData" :rules="formRules" ref="dataForm">
-            <el-form-item label="角色名称" prop="name">
-                <el-input v-model="formData.name" placeholder="请输入角色名称" auto-complete="off"></el-input>
+            <el-form-item label="角色名称" prop="roleName">
+                <el-input v-model="formData.roleName" placeholder="请输入角色名称" auto-complete="off"></el-input>
             </el-form-item>
             <el-form-item label="状态" prop="status">
                 <el-radio-group v-model="formData.status">
-                    <el-radio :label="0">禁用</el-radio>
+                		<el-radio :label="0">禁用</el-radio>
                     <el-radio :label="1">正常</el-radio>
                 </el-radio-group>
             </el-form-item>
             <el-form-item label="描述">
-                <el-input type="textarea" placeholder="请输入角色详细描述" v-model="formData.remark"></el-input>
+                <el-input type="textarea" placeholder="请输入角色详细描述" v-model="formData.roleRemark"></el-input>
             </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -116,52 +107,30 @@
 <script>
 import config from '@/api/bgapi'
 import request from '@/utils/request'
-import Pagination from '@/components/Pagination'
-const formJson = {
-    id: "",
-    name: "",
-    status: 1,
-    remark: ""
-};
+
+const formJson = {roleName: '', status: 0, roleRemark: ''};
 export default {
-  components: { Pagination },
   filters: {
     statusFilterType(status) {
-      const statusMap = {
-        0: 'gray',
-        1: 'success',
-        2: 'danger'
-      }
+      const statusMap = {0: 'danger',1: 'success'}
       return statusMap[status]
     },
     statusFilterName(status) {
-      const statusMap = {
-        0: '禁用',
-        1: '正常',
-        2: '未验证'
-      }
+      const statusMap = {0: '禁用',1: '正常'}
       return statusMap[status]
     }
   },
   data() {
     return {
       loading: false,
-      listQuery: {
-        page: 1,
-        page_size: 20,
-        name: '',
-      //  status: ''
-      },
+      listQuery: {roleName: ''},
       list: [],
       total:0,
 			formVisible:false,
 			formData:formJson,
 			formRules: {
-					name: [
+					roleName: [
 							{ required: true, message: "请输入名称", trigger: "blur" }
-					],
-					status: [
-							{ required: true, message: "请选择状态", trigger: "change" }
 					]
 			},
 			index:null,
@@ -170,7 +139,7 @@ export default {
       authFormVisible:false,
       defaultProps: {
           children: 'children',
-          label: 'title'
+          label: 'menuTitle'
       }, 
       authDefaultCheckedKeys:[],
       authList:[],
@@ -180,23 +149,22 @@ export default {
   },
   mounted(){
     //this.authFormData = {id:"",auth_rules:[]}
-    //this.getList();
+    this.getList();
   },
   methods: {
     async getList(){
       this.loading = true;
-      var res = await request.get(config.listUrl,{params:this.listQuery});
+      var res = await request.get(config.adminRoles,{params:this.listQuery});
       this.loading = false;
-      this.list = res.data.list || [];
-      this.total = res.data.total || 0;
+      this.list = res.data || [];
     },
     async querySearch(queryString, cb) {
-      var res = await request.get(config.listUrl,{params:this.listQuery});
-      var restaurants = res.data.list;
+      var res = await request.get(config.adminRoles,{params:this.listQuery});
+      var restaurants = res.data;
       cb(restaurants);
     },
     handleSelect(item) {
-      this.listQuery.name = item.name;
+      this.listQuery.roleName = item.roleName;
       this.getList();
     },    
 		// 表单弹窗
@@ -224,24 +192,31 @@ export default {
           this.formLoading = true;
           if(this.index !== null){
             //编辑
-            var res = await request.post(config.editRoleUrl,this.formData);
+						var id = this.formData.id;
+						var _data = {
+							roleName:this.formData.roleName,
+							roleRemark:this.formData.roleRemark,
+							status:this.formData.status
+						}
+            var res = await request.put(config.adminRoles+'/'+id,_data);
             if(res.code == '20000'){
               this.formLoading = false;
               this.formVisible = false;
               this.$message.success("操作成功");              
-              this.list.splice(this.index, 1, res.data);
+              this.getList();
             }else{
 							this.formLoading = false;
               this.formVisible = false;							
 						}
           }else{
             //新增
-            var res = await request.post(config.addRoleUrl,this.formData);
+						console.log(this.formData);
+            var res = await request.post(config.adminRoles,this.formData);
             if(res.code == '20000'){
               this.formLoading = false;
               this.formVisible = false;
               this.$message.success("操作成功");              
-              this.list.push(res.data);  
+              this.getList();  
             }else{
 							this.formLoading = false;
               this.formVisible = false;
@@ -258,14 +233,13 @@ export default {
         this.$refs["dataForm"].resetFields();
       }
     },    		
-		async handleAuth(id){
-      this.authFormData.id = id;
+		async handleAuth(row){
       this.authFormVisible = true;
-      var res = await request.get(config.getAuthListUrl,{params:{'id':id}});
+      var res = await request.get(config.menu);
       if(res.code == '20000'){
         this.treeLoad = false;
-        this.authList = res.data.auth_list;
-        this.authDefaultCheckedKeys = res.data.checked_keys;
+        this.authList = res.data;
+        this.authDefaultCheckedKeys = row.roleKeys;
       }
 		},
 		handleDel(index, row){
@@ -274,7 +248,7 @@ export default {
             type: "warning",
         })
         .then(async () => {
-            var res = await request.post(config.delRoleUrl,{'id':row.id});
+            var res = await request.delete(config.adminRoles+'/'+row.id);
             if(res.code == '20000'){
               this.$message.success("删除成功");  
               this.list.splice(index, 1);	
@@ -293,7 +267,8 @@ export default {
     async authFormSubmit(){
       this.authLoading = true;
       var checkedKeys = this.$refs.tree.getCheckedKeys();
-      this.authFormData.auth_rules = checkedKeys.join(',');
+      
+
 			if (this.authFormData && this.authFormData.auth_rules.length == 0) {
           this.$alert("请至少选择一个权限", "提示", {
               confirmButtonText: "确定"
@@ -301,12 +276,13 @@ export default {
           this.authLoading = false;
           return false;
       }
-      var res = await request.post(config.setAuthUrl,this.authFormData); 
+      var res = await request.put(config.adminRoles,this.authFormData); 
       this.authLoading = false;
       this.authFormVisible = false;             
       if(res.code == '20000'){
         this.$message.success("授权成功");   
       }
+
     },
     hideAutoForm(){
       this.authFormVisible = !this.authFormVisible;
